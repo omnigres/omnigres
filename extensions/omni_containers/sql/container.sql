3fbaec5d7df20b5625dae1842ce6b38664d5f04c
Yurii Rashkovskii <yrashk@gmail.com>
2022-12-27 15:02:20 -0800

    Problem: can't configure Containers
    Solution: allow passing options to docker_container_create

--- extensions/omni_containers/sql/container.sql
+++ extensions/omni_containers/sql/container.sql
@@ -9,5 +9,13 @@ SELECT omni_containers.docker_container_inspect(id)->'State'->'ExitCode' AS exit
 SET client_min_messages TO NOTICE;
 SELECT count(*) FROM pg_catalog.pg_tables WHERE tablename = 'container_test';
 
+-- Passing extra options
+WITH container AS (SELECT * FROM omni_containers.docker_container_create('ghcr.io/yrashk/psql', cmd => $$echo $TEST$$,
+                                                      options => $${"Env": ["TEST=ok"]}$$,
+                                                      wait => true, pull => true) AS id)
+SELECT omni_containers.docker_container_inspect(id)->'State'->'ExitCode' AS exit_code,
+       omni_containers.docker_container_logs(id) AS logs
+       FROM container;
+
 -- Creating a container that can't be found
 SELECT true FROM omni_containers.docker_container_create('cannotbefound');
