5f8d4136019b28ad3b9a0bcd90b405e5a2a19238
Yurii Rashkovskii <yrashk@gmail.com>
2023-03-03 06:15:03 -0800

    Problem: make fix-format doesn't work on NixOS
    Format.cmake reports that clang-format and/or Python are not found.
    
    Solution: include these in the Nix flake
    
    Also, use the mainstream repo for nixpkgs as cmake 3.25.1 has been
    merged.

--- flake.nix
+++ flake.nix
@@ -1,18 +1,18 @@
 {
   inputs = {
-    # Pending cmake 3.25.1 merge and release: https://github.com/NixOS/nixpkgs/pull/206799 
-    cmake_nixpkgs.url = "github:yrashk/nixpkgs?ref=cmake-3.25.1";
+    nixpkgs_master.url = "github:NixOS/nixpkgs";
     nixpkgs.url = "nixpkgs/nixos-22.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, cmake_nixpkgs, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs_master, nixpkgs, flake-utils }:
   flake-utils.lib.eachDefaultSystem
   (system:
-      let cmake = cmake_nixpkgs.legacyPackages.${system}.cmake; in
+      let cmake = nixpkgs_master.legacyPackages.${system}.cmake; in
+      let clang-tools = nixpkgs_master.legacyPackages.${system}.clang-tools_15; in
       let pkgs = nixpkgs.legacyPackages.${system};
       in {
         devShell = pkgs.mkShell { buildInputs = [ pkgs.pkgsStatic.openssl pkgs.pkgconfig cmake pkgs.flex pkgs.readline
-                                                  pkgs.zlib]; };
+                                                  pkgs.zlib clang-tools pkgs.python]; };
       });
 }
