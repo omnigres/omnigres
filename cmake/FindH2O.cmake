580330f3955f90ac74453c8d7d90d4eddd336e56
Yurii Rashkovskii <yrashk@gmail.com>
2023-03-13 08:39:43 -0700

    Problem: h2o dependency is not up-to-date
    Upstream has updated neverbleed dependency.
    
    Solution: update h2o version

--- cmake/FindH2O.cmake
+++ cmake/FindH2O.cmake
@@ -6,6 +6,6 @@ list(APPEND _h2o_options "WITH_MRUBY OFF")
 list(APPEND _h2o_options "DISABLE_LIBUV ON")
 
 cmake_policy(SET CMP0042 NEW)
-CPMAddPackage(NAME h2o GIT_REPOSITORY https://github.com/h2o/h2o GIT_TAG 9ab3feb VERSION 2.3.0-9ab3feb OPTIONS "${_h2o_options}")
+CPMAddPackage(NAME h2o GIT_REPOSITORY https://github.com/h2o/h2o GIT_TAG 1c8407e VERSION 2.3.0-1c8407e OPTIONS "${_h2o_options}")
 set_property(TARGET libh2o PROPERTY POSITION_INDEPENDENT_CODE ON)
 set_property(TARGET libh2o-evloop PROPERTY POSITION_INDEPENDENT_CODE ON)
