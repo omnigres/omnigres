3a2c5ebf7919ff17caaca4bc0906d20cbeca4827
Yurii Rashkovskii <yrashk@gmail.com>
2023-09-26 09:39:59 -0700

    Problem: OpenSSL deprecated API warnigns during h2o compile
    Not a big problem, but a nuisance.
    
    Solution: upgrade to the latest version of h2o
    
    See https://github.com/h2o/h2o/pull/3286

--- cmake/FindH2O.cmake
+++ cmake/FindH2O.cmake
@@ -6,6 +6,6 @@ list(APPEND _h2o_options "WITH_MRUBY OFF")
 list(APPEND _h2o_options "DISABLE_LIBUV ON")
 
 cmake_policy(SET CMP0042 NEW)
-CPMAddPackage(NAME h2o GIT_REPOSITORY https://github.com/h2o/h2o GIT_TAG d5b5721 VERSION 2.3.0-d5b5721 OPTIONS "${_h2o_options}")
+CPMAddPackage(NAME h2o GIT_REPOSITORY https://github.com/h2o/h2o GIT_TAG 4c84bfa VERSION 2.3.0-4c84bfa OPTIONS "${_h2o_options}")
 set_property(TARGET libh2o PROPERTY POSITION_INDEPENDENT_CODE ON)
 set_property(TARGET libh2o-evloop PROPERTY POSITION_INDEPENDENT_CODE ON)
