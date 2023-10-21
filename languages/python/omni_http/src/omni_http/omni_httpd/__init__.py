f8d2e2e440da27ce5fcfce5146cc59a730e5581d
Yurii Rashkovskii <yrashk@gmail.com>
2023-10-21 12:44:56 -0700

    Problem: impossible to answer HTTP request with Python
    Solution: integrate omni_httpd with omni_python experience
    
    This change introduces omni_http Python package that integrates with
    some parts of Omnigres HTTP stack and provides an adapter for Flask
    applications.
    
    It is fairly crude and is more of a proof of concept at this point.

---
+++ languages/python/omni_http/src/omni_http/omni_httpd/__init__.py
@@ -0,0 +1,47 @@
+import json
+from omni_python import Composite, Custom
+from ..omni_http import HttpHeader
+from typing import TypedDict, Optional, Any, Self
+from dataclasses import dataclass
+
+import sys
+
+@dataclass
+class HTTPRequest:
+    method: str
+    path: str
+    query_string: str
+    body: Optional[bytes]
+    headers: list[HttpHeader]
+
+    def wsgi_environ(self) -> dict:
+        environ = {'wsgi.version': (1, 0), 'wsgi.url_scheme': 'http', 'wsgi.input': self.body or b"",
+                   'wsgi.multithread': False,
+                   'REQUEST_METHOD': self.method, 'PATH_INFO': self.path, 'QUERY_STRING': self.query_string or ''}
+        for header in self.headers:
+            environ[f"HTTP{header['name'].upper()}"] = header['value']
+        return environ
+
+    @classmethod
+    def __pg_type_hint__(cls):
+        from omni_python import Composite, Custom
+        return Composite(cls, "omni_httpd.http_request")
+
+HTTPOutcome = Custom(Any, "omni_httpd.http_outcome")
+
+@dataclass
+class HTTPResponse:
+    headers: list[HttpHeader] = None
+    body: bytes = b""
+    status: int = 200
+
+    def outcome(self: Self) -> HTTPOutcome:
+      __plpy = sys.modules['__main__'].plpy
+      return __plpy.execute(__plpy.prepare("select omni_httpd.http_response(body => $1, status => $2, headers => $3) as result",
+                                           ["bytea", "int","omni_http.http_headers"]), [self.body, self.status, self.headers])[0]["result"]
+
+
+    @classmethod
+    def __pg_type_hint__(cls):
+        from omni_python import Composite, Custom
+        return Composite(cls, "omni_httpd.http_response")
