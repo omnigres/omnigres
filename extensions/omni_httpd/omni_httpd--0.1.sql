d0e2e6e13d27efbc75f145dab4c053eca5a760c1
Yurii Rashkovskii <yrashk@gmail.com>
2023-10-24 08:57:21 -0700

    Problem: omni_httpd.no_init still creates handlers/listeners
    These shouldn't be created in this case.
    
    Solution: ensure they aren't

--- extensions/omni_httpd/omni_httpd--0.1.sql
+++ extensions/omni_httpd/omni_httpd--0.1.sql
@@ -289,10 +289,17 @@ with
              coalesce(current_setting('omni_httpd.init_listen_address', true),
                       '0.0.0.0')::inet                                                 as init_listen_address,
              coalesce(current_setting('omni_httpd.init_port', true)::port, 8080::port) as init_port),
-    listener as (insert into listeners (address, port) select init_listen_address, init_port from config returning id),
-    handler as (insert into handlers (query) values
+    listener
+        as (insert into listeners (address, port) select init_listen_address, init_port from config where should_init returning id),
+    handler as (insert into handlers (query) (select
+                                                  query
+                                              from
+                                                  (values
                                                  ($$
-       SELECT omni_httpd.default_page(request.*::omni_httpd.http_request) FROM request $$) returning id)
+       SELECT omni_httpd.default_page(request.*::omni_httpd.http_request) FROM request $$)) v(query),
+                                                                                            config
+                                              where
+                                                  should_init) returning id)
 insert
 into
     listeners_handlers (listener_id, handler_id)
