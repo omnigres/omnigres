4a756e594d2c90e32916e43e1850eb5399037e7f
Yurii Rashkovskii <yrashk@gmail.com>
2023-11-07 06:37:19 -0800

    Problem: omni_schema may load sources before dependencies are ready
    Solution: introduce tools/language ordering priority
    while sorting files by name for consistency
    
    Closes #327

--- extensions/omni_schema/migrate/2_aux_tools.sql
+++ extensions/omni_schema/migrate/2_aux_tools.sql
@@ -4,13 +4,14 @@ create table auxiliary_tools
     filename_stem       text,
     filename_extension  text,
     processor           name not null,
-    processor_extension name
+    processor_extension name,
+    priority            integer not null default 0
 );
 
 insert
 into
-    auxiliary_tools (filename_stem, filename_extension, processor, processor_extension)
+    auxiliary_tools (filename_stem, filename_extension, processor, processor_extension, priority)
 values
-    ('requirements', 'txt', 'omni_python.install_requirements', 'omni_python');
+    ('requirements', 'txt', 'omni_python.install_requirements', 'omni_python', 1);
 
 select pg_catalog.pg_extension_config_dump('auxiliary_tools', '');
