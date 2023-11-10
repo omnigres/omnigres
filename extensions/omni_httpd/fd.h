4f12033bcc02f4aaa1ad304d70cc3d168f88b538
Yurii Rashkovskii <yrashk@gmail.com>
2023-11-10 08:08:36 -0800

    Problem: omni_httpd workers allocate new fds/listeners
    Every time there is a configuration reload, they seem to set up new
    listeners with new socket fds.
    
    The effects of these were as brutal as experiencing crashes in certain
    cases.
    
    Solution: ensure we compare fds by master fds
    
    Master fd is an fd assign to the socket in the master worker. This way
    we don't provision new sockets / listeners unless absolutely necessary.

--- extensions/omni_httpd/fd.h
+++ extensions/omni_httpd/fd.h
@@ -9,6 +9,29 @@
 #define i_tag fd
 #include <stc/cvec.h>
 
+/**
+ * This structure keeps a file descriptor and its master descriptor id for
+ * uniqueness comparison.
+ */
+typedef struct {
+  int fd;
+  int master_fd;
+} fd_fd;
+
+inline int fd_fd_cmp(const fd_fd *fd1, const fd_fd *fd2) {
+  int fd = fd1->fd - fd2->fd;
+  if (fd == 0) {
+    return fd1->master_fd - fd2->master_fd;
+  } else {
+    return fd;
+  }
+}
+
+#define i_key fd_fd
+#define i_tag fd_fd
+#define i_cmp fd_fd_cmp
+#include <stc/cvec.h>
+
 #ifndef SCM_MAX_FD
 #define SCM_MAX_FD 253
 #endif
@@ -19,6 +42,6 @@
 #define MAX_N_FDS SCM_MAX_FD
 
 int send_fds(int sock, cvec_fd *fds);
-cvec_fd recv_fds(int sock);
+cvec_fd_fd recv_fds(int sock);
 
 #endif // OMNI_HTTPD_FD_H
