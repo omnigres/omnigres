fa10e4d96510231333102bc15e8d9e648960daee
Yurii Rashkovskii <yrashk@gmail.com>
2023-11-14 07:21:26 -0800

    Problem: docker-related tests failing on macOS in GitHub Actions
    Error messages suggest that the IP address set up in DOCKER_HOST_IP is
    not correct.
    
    Solution: replace hard-coded IP address with a script

--- extensions/omni_containers/migrate/5_docker_container_stop.sql
+++ extensions/omni_containers/migrate/5_docker_container_stop.sql
@@ -4,25 +4,25 @@ create function docker_container_stop(id text)
 as
 $$
 declare
-response omni_httpc.http_response;
+    response omni_httpc.http_response;
 begin
-    select * into response from omni_httpc.http_execute(
-        omni_httpc.http_request(
-            format('http://%s/containers/%s/stop', omni_containers.docker_api_base_url(), id),
-            'POST'
-        )
-    );
+    select *
+    into response
+    from
+        omni_httpc.http_execute(
+                omni_httpc.http_request(
+                        format('http://%s/containers/%s/stop', omni_containers.docker_api_base_url(), id),
+                        'POST'
+                    )
+            );
     case response.status
         when 204 then -- We're done
-            return;
-        when 304 then
-            raise exception 'Can''t stop the container' using
+        return;
+        when 304 then raise exception 'Can''t stop the container' using
             detail = 'Container already stopped';
-        when 404 then
-            raise exception 'Can''t stop the container' using
+        when 404 then raise exception 'Can''t stop the container' using
             detail = 'No such container';
-        else
-            raise exception 'Can''t stop the container';
-    end case;
+        else raise exception 'Can''t stop the container: % (status % error: %)', convert_from(response.body, 'utf8'), response.status, response.error;
+        end case;
 end;
 $$ language plpgsql;
