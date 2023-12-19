485454a229d205191e178c1384e3b6c40b0e7d85
Yurii Rashkovskii <yrashk@gmail.com>
2023-12-19 12:16:21 -0800

    Problem: updating omni_txn variables implementation
    Should we do it? Or should we simply delegate to omni_var?
    
    Solution: delegate to omni_var pending future removal
    
    This makes maintenance easier.

--- extensions/omni_txn/migrate/1_variable.sql
+++ extensions/omni_txn/migrate/1_variable.sql
@@ -1,9 +1,21 @@
-create function set_variable(name name, value anyelement) returns anyelement language c
-as 'MODULE_PATHNAME';
+create function set_variable(name name, value anyelement) returns anyelement
+    language plpgsql
+    security definer -- to be able to access omni_var
+as
+$$
+begin
+    raise warning 'omni_txn.set_variable is deprecated, use omni_var.set';
+    return omni_var.set(name, value);
+end
+$$;
 
 create function get_variable(name name, default_value anyelement) returns anyelement
-    language c
-    stable
-as
-'MODULE_PATHNAME';
-
+    language plpgsql
+    security definer -- to be able to access omni_var
+    stable as
+$$
+begin
+    raise warning 'omni_txn.get_variable is deprecated, use omni_var.get';
+    return omni_var.get(name, default_value);
+end
+$$;
