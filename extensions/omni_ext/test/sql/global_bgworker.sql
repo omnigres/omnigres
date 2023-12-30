f990594d4cecb46da7c37edd356df9ebd533194e
idoko <okokomichaels@outlook.com>
2023-12-30 11:16:28 +0100

    Problem: MultiDB tests still uses pg_regress
    Solution: Convert tests across databases to use pg_yregress.

--- extensions/omni_ext/test/sql/global_bgworker.sql
+++
@@ -1,2 +0,0 @@
-SELECT omni_ext_test.wait_for_table('global_worker_started');
-\d global_worker_started;
--- extensions/omni_ext/test/sql/load_version.sql
+++
@@ -1,2 +0,0 @@
-SELECT omni_ext.load('omni_ext_test', 'unknown');
-SELECT omni_ext.load('omni_ext_test', '0.1');
