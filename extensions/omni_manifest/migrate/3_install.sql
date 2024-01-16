47a8f5a9641447bb733ab605a36ee51e60fbf357
Yurii Rashkovskii <yrashk@gmail.com>
2024-01-15 20:30:07 -0800

    Problem: unclear how to use omni_manifest
    This severely restricts its usefulness.
    
    Solution: write some documentation

--- extensions/omni_manifest/migrate/3_install.sql
+++ extensions/omni_manifest/migrate/3_install.sql
@@ -1,8 +1,8 @@
-create type artifact_status as enum ('installed', 'missing', 'updated');
+create type requirement_status as enum ('installed', 'missing', 'updated');
 
 create type install_report as
 (
     requirement requirement,
-    status      artifact_status
+    status      requirement_status
 );
 /*{% include "../src/install.sql" %}*/
