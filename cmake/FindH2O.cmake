065daee203ef3d77ad467729d52b8927d6c2101d
Yurii Rashkovskii <yrashk@gmail.com>
2024-03-04 06:58:36 -0800

    Problem: h2o is slightly outdated
    There have been some useful and interesting fixes in h2o recently, for
    example:
    
    * https://github.com/h2o/h2o/pull/3359
    * https://github.com/h2o/h2o/pull/3360
    * https://github.com/h2o/h2o/pull/3361
    * https://github.com/h2o/h2o/pull/3356
    
    Solution: update to the latest version

--- cmake/FindH2O.cmake
+++ cmake/FindH2O.cmake
@@ -6,6 +6,6 @@ list(APPEND _h2o_options "WITH_MRUBY OFF")
 list(APPEND _h2o_options "DISABLE_LIBUV ON")
 
 cmake_policy(SET CMP0042 NEW)
-CPMAddPackage(NAME h2o GIT_REPOSITORY https://github.com/h2o/h2o GIT_TAG 1d81c6e VERSION 2.3.0-1d81c6e OPTIONS "${_h2o_options}")
+CPMAddPackage(NAME h2o GIT_REPOSITORY https://github.com/h2o/h2o GIT_TAG 579135a VERSION 2.3.0-579135a OPTIONS "${_h2o_options}")
 set_property(TARGET libh2o PROPERTY POSITION_INDEPENDENT_CODE ON)
 set_property(TARGET libh2o-evloop PROPERTY POSITION_INDEPENDENT_CODE ON)
