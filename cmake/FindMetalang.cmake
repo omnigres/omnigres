d466170dbb02bfe25faa1b3b514d69a4714feca8
steve-chavez <stevechavezast@gmail.com>
2024-06-09 18:34:32 -0500

    Problem: cmake setup is not suitable for packaging
    Currently the cmake setup forces `CPMAddPackage` for various dependencies,
    it doesn't allow to get them from a package mananger.
    
    This makes it hard to create a Nix package of omnigres, for one. Other
    Linux distributions will have the same problem.
    
    Solution: Try to get dependencies from the system and if not available
    use `CPMAddPackage`.
    
    Some dependencies can be found with `find_package` and others with
    `pkg_check_modules`.
    
    List of dependencies covered:
    
    - [x] pugixml
    - [x] uriparser
    - [x] h2o
    - [x] libfyaml
    - [x] stc
    - [x] metalang99

--- cmake/FindMetalang.cmake
+++ cmake/FindMetalang.cmake
@@ -1,3 +1,8 @@
 include(CPM)
+include(FindPkgConfig)
 
-CPMAddPackage("gh:Hirrolot/metalang99@1.13.2")
+pkg_check_modules(metalang99 metalang99)
+
+if(NOT metalang99_FOUND)
+  CPMAddPackage("gh:Hirrolot/metalang99@1.13.2")
+endif()
