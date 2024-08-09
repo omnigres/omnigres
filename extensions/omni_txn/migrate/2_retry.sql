b9798409f007a6941dc6ad7ef2bccc6ac5cc7ba8
Eduardo Bellani <ebellani@gmail.com>
2024-08-09 15:21:06 -0300

    Problem: Retries can perform poorly under high contention.
    > The simplest of these contention cases is when a whole lot of clients
    > start at the same time, and try to update the same database row. With
    > one client guaranteed to succeed every round, the time to complete all
    > the updates grows linearly with contention ... N clients compete in
    > the first round, N-1 in the second round, and so on. Having every
    > client compete in every round is wasteful. [1]
    
    Solution: Add backoff with jitter to spread the calls over time,
    minimizing contention.
    
    [1] https://aws.amazon.com/blogs/architecture/exponential-backoff-and-jitter/

--- extensions/omni_txn/migrate/2_retry.sql
+++ extensions/omni_txn/migrate/2_retry.sql
@@ -1,9 +1,9 @@
-create procedure retry(stmts text, max_attempts int default 10, repeatable_read boolean default false)
+create procedure retry(stmts text, max_attempts int default 10, repeatable_read boolean default false, gather_backoff_values boolean default false)
     language c as
 'MODULE_PATHNAME';
 
 comment on procedure retry is $$
-Retry serializable transaction on statements `stmts`, `max_attempt` number of times (10 by default)
+Retry serializable transaction on statements `stmts`, `max_attempt` number of times (10 by default). `gather_backoff_values` controls if the backoff values used for sleeping will be recorded for debugging/testing purposes (false in orded to increase performance)
 $$;
 
 create function current_retry_attempt()
@@ -13,4 +13,18 @@ create function current_retry_attempt()
 
 comment on function current_retry_attempt() is $$
 Within `omni_txn.retry` indicates the current retry attempt. Zero during the first run.
+$$;
+
+create function retry_backoff_values()
+    returns table
+            (
+              backoff_sleep_time int
+            )
+    volatile
+    language c
+as
+'MODULE_PATHNAME';
+
+comment on function retry_backoff_values () is $$
+Retrieves the backoff values used in the `omni_txn.retry` run. Empty in the first run.
 $$;
