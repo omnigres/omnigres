adf0b94fd05895df62fbe76660d1156b4d227ae3
Yurii Rashkovskii <yrashk@gmail.com>
2024-08-23 08:47:26 -0700

    Problem: `regproc[edure]` have limited usefulness
    Sure, they denote that an OID is a function/procedure, and, in case of
    `regprocedure`, that function has certain arguments.
    
    But the type itself does not convey the signature, which makes it not
    possible for us to call these function.
    
    Solution: design "function signature types"
    
    This type is also an OID but we also store information about its full
    signature and we validate on casts that the functions match the entire
    signature.
    
    These makes these types represent the actual function signature, making
    them callable. One can compose them into interfaces through composite
    types.

---
+++ extensions/omni_types/docs/function_signature_types.md
@@ -0,0 +1,59 @@
+# Function Signature Types
+
+Function signature types are essentially a way to type functions with a specific arguments and return type signature.
+Whereas `regproc` and `regprocedure` denote that the type is a function or a procedure, the type itself does not convey
+its signature. This limits their usefulness.
+
+Function signature types, on the other hand, can be used to call functions that match the signature.
+
+## Defining a function signature type
+
+To define such a type, use `omni_types.function_signature_type(name, ...signature)`:
+
+```postgresql
+select omni_types.function_signature_type('sig', 'text', 'int')
+```
+
+In the above example, it will define type `sig` that denotes a function that takes `text` and returns `int` (the last
+element in the variadic `signature` argument is the return type)
+
+| *Parameter* | *Type*           | *Description*                                            |
+|------------:|------------------|----------------------------------------------------------|
+|      *name* | name             | Name of the newly created type                           |
+| *signature* | variadic regtype | Signature of the function. Last value is the return type |
+
+Calling the function with the same arguments will return the same type without attempting to recreate it, if the
+signatures match. Otherwise, an error will occur.
+
+### Defining a signature from a prototype
+
+One can also define a function signature type by using an existing function as a prototype
+with `omni_types.function_signature_type(name, function)`:
+
+```postgresql
+select omni_types.function_signature_type_of('sig', 'length(text)')
+```
+
+| *Parameter* | *Type* | *Description*                                                                  |
+|------------:|--------|--------------------------------------------------------------------------------|
+|      *name* | name   | Name of the newly created type                                                 |
+|      *func* | text   | Name (`pg_backend_pid`) or argument signature (`length(text)`) of the function |
+
+## Casting to a function signature type
+
+With the type defined, one can cast a function name into it. It will return a `sig` type successfully if it matches:
+
+```postgresql
+select 'length'::sig
+```
+
+## Calling a function
+
+Function signature types can be called (and it is one of its primary benefits!) by using an auto-generated `call_<NAME>`
+function:
+
+```postgresql
+select call_sig('length', 'hello')
+-- returns 5 as this is what `length(text)` would return
+```
+
---
+++ extensions/omni_types/funsig_type.c
@@ -0,0 +1,160 @@
+/**
+ * @file funsig_type.c
+ *
+ */
+
+// clang-format off
+#include <postgres.h>
+#include <fmgr.h>
+// clang-format on
+#include <access/heapam.h>
+#include <access/htup_details.h>
+#include <access/table.h>
+#include <catalog/namespace.h>
+#include <catalog/pg_cast.h>
+#include <catalog/pg_collation.h>
+#include <catalog/pg_language.h>
+#include <catalog/pg_proc.h>
+#include <catalog/pg_type.h>
+#include <commands/typecmds.h>
+#include <executor/spi.h>
+#include <miscadmin.h>
+#include <utils/array.h>
+#include <utils/builtins.h>
+#include <utils/fmgroids.h>
+#include <utils/lsyscache.h>
+#include <utils/syscache.h>
+
+#include "sum_type.h"
+
+PG_FUNCTION_INFO_V1(function_signature_in);
+
+Datum function_signature_in(PG_FUNCTION_ARGS) {
+  char *input = PG_GETARG_CSTRING(0);
+
+  // Get the type that we need to return
+  Oid typeoid = get_func_rettype(fcinfo->flinfo->fn_oid);
+
+  // Find matching record in our table
+  Oid types = get_relname_relid("function_signature_types", get_namespace_oid("omni_types", false));
+  Relation rel = table_open(types, AccessShareLock);
+  TupleDesc tupdesc = RelationGetDescr(rel);
+  TableScanDesc scan = table_beginscan_catalog(rel, 0, NULL);
+  Oid matching_function = InvalidOid;
+  for (;;) {
+    HeapTuple tup = heap_getnext(scan, ForwardScanDirection);
+    // The end
+    if (tup == NULL)
+      break;
+
+    bool isnull;
+    // Matching type
+    // TODO: search by index
+    if (typeoid == DatumGetObjectId(heap_getattr(tup, 1, tupdesc, &isnull))) {
+
+      AnyArrayType *arguments = DatumGetAnyArrayP(heap_getattr(tup, 2, tupdesc, &isnull));
+
+      // If argument lengths are not matching, move on
+      int nargs = ArrayGetNItems(AARR_NDIM(arguments), AARR_DIMS(arguments));
+
+      List *names = list_make1(makeString(input));
+      FuncCandidateList candidates = FuncnameGetCandidates(names, nargs, NIL, false, false,
+#if PG_MAJORVERSION_NUM > 13
+                                                           false,
+#endif
+                                                           true);
+
+      while (candidates) {
+        // Iterate through arguments
+        ArrayIterator it = array_create_iterator((ArrayType *)arguments, 0, NULL);
+
+        Datum elem;
+        // Iterate through argument types
+        int i = 0;
+        while (array_iterate(it, &elem, &isnull)) {
+          if (isnull) {
+            continue;
+          }
+          if (DatumGetObjectId(elem) != candidates->args[i]) {
+            goto done_iter;
+          }
+          i++;
+        }
+        // Get return types of the looked up function
+        HeapTuple proc_tuple = SearchSysCache1(PROCOID, ObjectIdGetDatum(candidates->oid));
+        Assert(HeapTupleIsValid(proc_tuple));
+        Form_pg_proc proc_struct = (Form_pg_proc)GETSTRUCT(proc_tuple);
+        Oid function_ret_type = proc_struct->prorettype;
+        ReleaseSysCache(proc_tuple);
+
+        if (function_ret_type != DatumGetObjectId(heap_getattr(tup, 3, tupdesc, &isnull))) {
+          goto done_iter;
+        }
+
+        matching_function = candidates->oid;
+      done_iter:
+        array_free_iterator(it);
+        if (OidIsValid(matching_function)) {
+          break;
+        }
+        candidates = candidates->next;
+      }
+    }
+  }
+  if (scan->rs_rd->rd_tableam->scan_end) {
+    scan->rs_rd->rd_tableam->scan_end(scan);
+  }
+  table_close(rel, AccessShareLock);
+  if (!OidIsValid(matching_function)) {
+    MemoryContext oldcontext = CurrentMemoryContext;
+    PG_TRY();
+    { DirectFunctionCall1(regprocin, CStringGetDatum(input)); }
+    PG_CATCH();
+    {
+      int errcode = geterrcode();
+      MemoryContextSwitchTo(oldcontext);
+      if (errcode != ERRCODE_AMBIGUOUS_FUNCTION) {
+        PG_RE_THROW();
+      } else {
+        FlushErrorState();
+      }
+    }
+    PG_END_TRY();
+    ereport(ERROR, errmsg("function \"%s\" does not match the signature of the type", input));
+  }
+  PG_RETURN_OID(matching_function);
+}
+
+PG_FUNCTION_INFO_V1(invoke);
+
+Datum invoke(PG_FUNCTION_ARGS) {
+  if (PG_ARGISNULL(0)) {
+    ereport(ERROR, errmsg("Can't invoke a NULL function"));
+  }
+
+  FmgrInfo flinfo;
+  MemoryContext oldcontext = CurrentMemoryContext;
+  PG_TRY();
+  { fmgr_info(PG_GETARG_OID(0), &flinfo); }
+  PG_CATCH();
+  {
+    MemoryContextSwitchTo(oldcontext);
+    FlushErrorState();
+    ereport(ERROR, errmsg("function does not exist"));
+  }
+  PG_END_TRY();
+
+  LOCAL_FCINFO(fcinfo1, FUNC_MAX_ARGS); // FIXME: space-inefficient
+
+  InitFunctionCallInfoData(*fcinfo1, &flinfo, fcinfo->nargs - 1, fcinfo->fncollation,
+                           fcinfo->context, fcinfo->resultinfo);
+
+  for (short i = 0; i < fcinfo->nargs - 1; i++) {
+    fcinfo1->args[i] = fcinfo->args[i + 1];
+    if (fcinfo1->flinfo->fn_strict && fcinfo1->args[i].isnull) {
+      PG_RETURN_NULL();
+    }
+  }
+
+  return FunctionCallInvoke(fcinfo1);
+}
