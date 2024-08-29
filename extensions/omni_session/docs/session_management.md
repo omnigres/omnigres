5e3a7bbc6ee0151687e80ec0ef5e2c833f7305e3
Yurii Rashkovskii <yrashk@gmail.com>
2024-08-29 11:58:46 +0300

    Problem: omni_session is a bit too HTTP-centric
    This means we can't really use the logic for other interaction modes.
    
    Solution: extract its logical core into a separate function

--- extensions/omni_session/docs/session_management.md
+++ extensions/omni_session/docs/session_management.md
@@ -4,3 +4,32 @@
 providing this functionality in cooperation with the HTTP stack, but theoretically it is not limited to it.
 
 This extensions creates an unlogged `omni_session.sessions` table that contains all sessions.
+
+## Session handler
+
+Function `omni_session.session_handler` is designed to handle session application to differently typed objects:
+
+### UUID
+
+`omni_session.session_handler(uuid)` returns `uuid` and represents the core of session functionality.
+
+* Given a null `uuid`, it creates a new session and sets `omni_session.session` transaction variable to it. Returns a
+  new session UUID.
+* Given a non-null `uuid` that is a valid existing session, it sets `omni_session.session` transaction variable to it.
+  Returns the same UUID.
+* Given a non-null `uuid` that is not a valid existing session, it creates a new session and sets `omni_session.session`
+  transaction variable to it. Returns a new session UUID.
+
+### HTTP Request
+
+`omni_session.session_handler(omni_httpd.http_request)` returns unmodified `omni_httpd.http_request` and retrieves the
+UUID from request's cookie called `_session`. Its behavior mirrors that of UUID behavior above.
+
+Accepts an optional `cookie_name` parameter to specify a different name for the cookie.
+
+### HTTP Response
+
+`omni_session.session_handler(omni_httpd.http_response)` returns a modified `omni_httpd.http_outcome` with a cookie
+`_session` set to `omni_session.session` transaction variable value.
+
+Accepts an optional `cookie_name` parameter to specify a different name for the cookie.
--- extensions/omni_session/src/session_handler.sql
+++ extensions/omni_session/src/session_handler.sql
@@ -1,16 +1,13 @@
-create function session_handler(request omni_httpd.http_request, cookie_name text default '_session') returns omni_httpd.http_request
+create function session_handler(session_id uuid) returns uuid
     language plpgsql as
 $$
 declare
     _session uuid;
-    _ins uuid;
-    _upd uuid;
+    _ins     uuid;
+    _upd     uuid;
 begin
-    with cookie
-             as (select omni_web.cookie_get(omni_http.http_header_get(request.headers, 'cookie'),
-                                            cookie_name)::uuid as value),
-         touch
-             as (update omni_session.sessions set touched_at = clock_timestamp() from cookie where uuid = cookie.value returning uuid),
+    with touch
+             as (update omni_session.sessions set touched_at = clock_timestamp() where uuid = session_id returning uuid),
          insertion as (
              insert
                  into omni_session.sessions (uuid)
@@ -18,9 +15,19 @@ begin
                      returning uuid)
     select omni_var.set('omni_session.session', coalesce(insertion.uuid, touch.uuid)), insertion.uuid, touch.uuid
     into _session, _ins, _upd
-    from cookie
+    from (values (null::uuid)) t (c)
              left join insertion on true
              left join touch on true;
+    return _session;
+end;
+$$;
+
+create function session_handler(request omni_httpd.http_request, cookie_name text default '_session') returns omni_httpd.http_request
+    language plpgsql as
+$$
+begin
+    perform omni_session.session_handler(omni_web.cookie_get(omni_http.http_header_get(request.headers, 'cookie'),
+                                                             cookie_name)::uuid);
     return request;
 end;
 $$;
