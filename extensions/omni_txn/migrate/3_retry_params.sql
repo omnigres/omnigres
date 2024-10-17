e62a191347ae373a34e64d06a6a43925c57cd91b
Yurii Rashkovskii <yrashk@gmail.com>
2024-10-17 10:03:09 -0700

    Problem: omni_txn.retry is rather inflexible
    We can only pass a string as a query, and that's it.
    
    Solution: allow the query to be parameterized
    
    This will allow preparing data separately from the query.

---
+++ extensions/omni_txn/migrate/3_retry_params.sql
@@ -0,0 +1,12 @@
+drop procedure retry(stmts text, max_attempts int, repeatable_read boolean,
+                     collect_backoff_values boolean);
+
+create procedure retry(stmts text, max_attempts int default 10, repeatable_read boolean default false,
+                       collect_backoff_values boolean default false,
+                       params record default null::record)
+    language c as
+'MODULE_PATHNAME';
+
+comment on procedure retry is $$
+Retry serializable transaction on statements `stmts`, `max_attempt` number of times (10 by default). `collect_backoff_values` controls if the backoff values used for sleeping will be recorded for debugging/testing purposes (false in orded to increase performance)
+$$;
