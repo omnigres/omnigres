1ade6ac90de25f20341be67b1ed564ba6cf67fbd
shubham yadav <126192924+yadavshubham01@users.noreply.github.com>
2024-10-24 20:23:36 +0530

    Update 2_retry.sql

--- extensions/omni_txn/migrate/2_retry.sql
+++ extensions/omni_txn/migrate/2_retry.sql
@@ -1,5 +1,5 @@
-create procedure retry(stmts text, max_attempts int default null, repeatable_read boolean default false,
-                       collect_backoff_values boolean default false , timeout interval default null )
+create procedure retry(stmts text, max_attempts int default 10, repeatable_read boolean default false,
+                       collect_backoff_values boolean default false )
     language c as
 'MODULE_PATHNAME';
 
