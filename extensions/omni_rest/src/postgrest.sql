e8193e5840a38f078b0ad1f80171d6278bbeaba9
Yurii Rashkovskii <yrashk@gmail.com>
2024-11-27 08:32:15 +0700

    Problem: unnecessary logging in omni_rest
    Solution: remove it

--- extensions/omni_rest/src/postgrest.sql
+++ extensions/omni_rest/src/postgrest.sql
@@ -4,7 +4,6 @@ as
 $$
 declare
 begin
-    raise notice '%',request;
     return coalesce(omni_rest.handler(omni_rest.postgrest_get_route(request)),
                     omni_rest.handler(omni_rest.postgrest_cors_route(request))
            );
