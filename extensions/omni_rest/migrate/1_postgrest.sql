9fdc8b5ec0875952126ed761cebe36c23d8b6bd7
Yurii Rashkovskii <yrashk@gmail.com>
2024-11-27 20:06:23 +0700

    Problem: omni_rest handlers
    They are a little bit over-engineered (using older proposal for routing
    messages) and are unable to handle multi-transactional processing.
    
    Solution: switch to procedure-based handlers
    
    These are simpler and can accommodate non-atomic needs of
    multi-transactional processing, or needs like transaction isolation
    level changes, retries, etc.

--- extensions/omni_rest/migrate/1_postgrest.sql
+++ extensions/omni_rest/migrate/1_postgrest.sql
@@ -1,29 +1,3 @@
-create type fallback_route as
-(
-    request omni_httpd.http_request
-);
-
-/*{% include "../src/fallback_route.sql" %}*/
-/*{% include "../src/handler_fallback_route.sql" %}*/
-
-create type postgrest_get_route as
-(
-    request  omni_httpd.http_request,
-    relation regclass
-);
-
-
-/*{% include "../src/postgrest_get_route.sql" %}*/
-/*{% include "../src/handler_postgrest_get_route.sql" %}*/
-
-create type postgrest_cors_route as
-(
-    request omni_httpd.http_request,
-    origin  text
-);
-
-/*{% include "../src/postgrest_cors_route.sql" %}*/
-/*{% include "../src/handler_postgrest_cors_route.sql" %}*/
-
-
+/*{% include "../src/postgrest_get.sql" %}*/
+/*{% include "../src/postgrest_cors.sql" %}*/
 /*{% include "../src/postgrest.sql" %}*/
