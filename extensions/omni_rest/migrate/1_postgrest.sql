ea47a638954a0a848ac77e66b1f1d8e7114bee6b
Yurii Rashkovskii <yrashk@gmail.com>
2024-12-11 15:16:18 +0700

    Problem: impossible to insert using omni_rest
    This is a major deficiency as it makes it much less useful and portable.
    
    Solution: implement core PostgREST support for insert
    
    Items still missing:
    
    * [URL-encoded
      payloads](https://docs.postgrest.org/en/v12/references/api/tables_views.html#x-www-form-urlencoded)
    * [CSV
      import](https://docs.postgrest.org/en/v12/references/api/tables_views.html#bulk-insert)
    * [Column
      specification](https://docs.postgrest.org/en/v12/references/api/tables_views.html#specifying-columns)

--- extensions/omni_rest/migrate/1_postgrest.sql
+++ extensions/omni_rest/migrate/1_postgrest.sql
@@ -1,3 +1,5 @@
+/*{% include "../src/__eval.sql" %}*/
 /*{% include "../src/postgrest_get.sql" %}*/
+/*{% include "../src/postgrest_insert.sql" %}*/
 /*{% include "../src/postgrest_cors.sql" %}*/
 /*{% include "../src/postgrest.sql" %}*/
