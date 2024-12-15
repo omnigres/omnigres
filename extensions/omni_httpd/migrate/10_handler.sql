2734584a6ba648981b9c923c62ccbc447ab764f4
Yurii Rashkovskii <yrashk@gmail.com>
2024-12-15 16:35:17 +0700

    Problem: omni_httpd does not work on Postgres 13
    It can't create a handler with an `out` parameter (unsupported)
    
    Solution: use `inout` to tolerate 13's deficiency
    
    Not sure if we should keep this designation after we remove support for
    Postgres 13.

--- extensions/omni_httpd/migrate/10_handler.sql
+++ extensions/omni_httpd/migrate/10_handler.sql
@@ -1,8 +1,12 @@
 -- Replace function with procedure
 drop function handler(int, http_request);
 
--- Empty implementation
-create procedure handler(int, http_request, out http_outcome)
+-- Empty implementation of the handler
+-- NOTE: we use `inout` here mostly to support Postgres 13
+-- (Postgres 13 does not support `out` in procedures)
+-- and we're currently passing `null` into it
+-- TODO: review this policy once we stop supporting Postgres 13
+create procedure handler(int, http_request, inout http_outcome)
     language plpgsql as
 $$
 begin
@@ -10,6 +14,6 @@ begin
 end;
 $$;
 
-comment on procedure handler(int, http_request, out http_outcome) is $$
+comment on procedure handler(int, http_request, inout http_outcome) is $$
 This procedure handlers incoming HTTP requests. Can't be deleted but can be replaced.
 $$;
