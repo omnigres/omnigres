0ced00c4336aee9284c3381269035fff25b7b3b8
Yurii Rashkovskii <yrashk@gmail.com>
2024-12-28 15:03:40 +0700

    Problem: omni_rest RPC code is hard to follow
    The indentation seem to be a bit off
    
    Solution: reformat the code

--- extensions/omni_rest/src/_postgrest_function.sql
+++ extensions/omni_rest/src/_postgrest_function.sql
@@ -1,52 +1,46 @@
-create or replace function _postgrest_function_by_arguments (namespace text, function_name text, function_arguments text[])
+create or replace function _postgrest_function_by_arguments(namespace text, function_name text, function_arguments text[])
     returns regproc[]
     language sql
     stable
-    as $$
-    with arguments as (
-        select
-            oid,
-            array_agg(coalesce(name, '')) filter (where idx <= (pronargs - pronargdefaults)) as required_parameters,
-        array_agg(coalesce(name, '')) as parameters
-    from
-        pg_proc,
-        unnest(proargnames, proargtypes, proargmodes)
-        with ordinality as _ (name, type, mode, idx)
-    where
-        type is not null -- only input arguments
-    group by
-        oid
-        -- we can only work with named arguments
-    having
-        count(*) - count(name) = 0
-)
-select
-    array_agg(p.oid::regproc)
-from
-    pg_proc p
-    left join arguments a on a.oid = p.oid
-    left join pg_catalog.pg_namespace n on n.oid = p.pronamespace
-where
-    n.nspname = namespace
-    and p.proname = function_name
-    and coalesce(required_parameters, '{}') <@ function_arguments
-    and (function_arguments <@ coalesce(parameters, '{}')
-        or function_arguments = coalesce(parameters, '{}'))
+as
+$$
+with arguments as (select oid,
+                          array_agg(coalesce(name, ''))
+                          filter (where idx <= (pronargs - pronargdefaults)) as required_parameters,
+                          array_agg(coalesce(name, ''))                      as parameters
+                   from pg_proc,
+                        unnest(proargnames, proargtypes, proargmodes)
+                            with ordinality as _ (name, type, mode, idx)
+                   where type is not null -- only input arguments
+                   group by oid
+                   -- we can only work with named arguments
+                   having count(*) - count(name) = 0)
+select array_agg(p.oid::regproc)
+from pg_proc p
+         left join arguments a on a.oid = p.oid
+         left join pg_catalog.pg_namespace n on n.oid = p.pronamespace
+where n.nspname = namespace
+  and p.proname = function_name
+  and coalesce(required_parameters, '{}') <@ function_arguments
+  and (function_arguments <@ coalesce(parameters, '{}')
+    or function_arguments = coalesce(parameters, '{}'))
 $$;
 
-create procedure _postgrest_function (request omni_httpd.http_request, function_reference inout regproc, namespace inout text, settings postgrest_settings default postgrest_settings ())
-language plpgsql
-as $$
+create procedure _postgrest_function(request omni_httpd.http_request, function_reference inout regproc,
+                                     namespace inout text, settings postgrest_settings default postgrest_settings())
+    language plpgsql
+as
+$$
 declare
     saved_search_path text := current_setting('search_path');
-    function_name text;
+    function_name     text;
     function_arguments text[];
-    all_references regproc[];
+    all_references    regproc[];
 begin
     if request.method = 'GET' then
-        namespace := omni_http.http_header_get (request.headers, 'accept-profile');
+        namespace := omni_http.http_header_get(request.headers, 'accept-profile');
     else
-        namespace := omni_http.http_header_get (request.headers, 'content-profile');
+        namespace := omni_http.http_header_get(request.headers, 'content-profile');
     end if;
     if namespace is null and cardinality(settings.schemas) > 0 then
         namespace := settings.schemas[1]::text;
@@ -62,27 +56,22 @@ begin
     perform
         set_config('search_path', namespace, true);
     function_name := split_part(request.path, '/', 3);
-    select
-        case request.method
-        when 'GET' then
-        (
-            select
-                array_agg(name)
-            from
-                unnest(omni_web.parse_query_string (request.query_string))
-                with ordinality as _ (name, i)
-            where
-                i % 2 = 1)
-        when 'POST' then
-        (
-            select
-                array_agg(jsonb_object_keys)
-            from
-                jsonb_object_keys(convert_from(request.body, 'utf-8')::jsonb))
-        end into function_arguments;
-    all_references := omni_rest._postgrest_function_by_arguments (namespace, function_name, coalesce(function_arguments, '{}'));
-    select
-        all_references[1] into function_reference;
+    select case request.method
+               when 'GET' then
+                   (select array_agg(name)
+                    from unnest(omni_web.parse_query_string(request.query_string))
+                             with ordinality as _ (name, i)
+                    where i % 2 = 1)
+               when 'POST' then
+                   (select array_agg(jsonb_object_keys)
+                    from
+                        jsonb_object_keys(convert_from(request.body, 'utf-8')::jsonb))
+               end
+    into function_arguments;
+    all_references :=
+            omni_rest._postgrest_function_by_arguments(namespace, function_name, coalesce(function_arguments, '{}'));
+    select all_references[1]
+    into function_reference;
     if function_reference is null or function_reference::text like 'pg_%' then
         function_reference := null;
         return;
--- extensions/omni_rest/src/postgrest_rpc.sql
+++ extensions/omni_rest/src/postgrest_rpc.sql
@@ -1,49 +1,49 @@
-create or replace function _postgrest_function_call_arguments (fn regproc, passed_arguments text[])
-    returns text immutable
+create or replace function _postgrest_function_call_arguments(fn regproc, passed_arguments text[])
+    returns text
+    immutable
     language sql
-    as $$
-    select
-        string_agg(format('%1$I => $%2$s :: %3$I', name, idx, t.typname), ', ' order by idx)
-    from
-        pg_proc p,
-        unnest(proargnames, proargtypes, proargmodes)
-    with ordinality as _ (name, type, mode, idx)
-    join pg_type t on t.oid = type
-where
-    type is not null
-    and p.oid = fn
-    and name::text = any (passed_arguments)
+as
+$$
+select string_agg(format('%1$I => $%2$s :: %3$I', name, idx, t.typname), ', ' order by idx)
+from pg_proc p,
+     unnest(proargnames, proargtypes, proargmodes)
+         with ordinality as _ (name, type, mode, idx)
+         join pg_type t on t.oid = type
+where type is not null
+  and p.oid = fn
+  and name::text = any (passed_arguments)
 $$;
 
-create or replace function _postgrest_function_ordered_argument_values (fn regproc, passed_arguments text[], passed_values jsonb)
-    returns jsonb immutable
+create or replace function _postgrest_function_ordered_argument_values(fn regproc, passed_arguments text[], passed_values jsonb)
+    returns jsonb
+    immutable
     language sql
-    as $$
-    select
-        jsonb_agg(passed_values ->> (name::text)
-        order by idx)
-    from
-        pg_proc p,
-        unnest(proargnames, proargtypes, proargmodes)
-    with ordinality as _ (name, type, mode, idx)
-    join pg_type t on t.oid = type
-where
-    type is not null
-    and p.oid = fn
-    and name::text = any (passed_arguments)
+as
+$$
+select jsonb_agg(passed_values ->> (name::text)
+                 order by idx)
+from pg_proc p,
+     unnest(proargnames, proargtypes, proargmodes)
+         with ordinality as _ (name, type, mode, idx)
+         join pg_type t on t.oid = type
+where type is not null
+  and p.oid = fn
+  and name::text = any (passed_arguments)
 $$;
 
-create procedure postgrest_rpc (request omni_httpd.http_request, outcome inout omni_httpd.http_outcome, settings postgrest_settings default postgrest_settings ())
-language plpgsql
-as $$
+create procedure postgrest_rpc(request omni_httpd.http_request, outcome inout omni_httpd.http_outcome,
+                               settings postgrest_settings default postgrest_settings())
+    language plpgsql
+as
+$$
 declare
     function_reference regproc;
-    query text;
-    namespace text;
-    result jsonb;
+    query              text;
+    namespace          text;
+    result             jsonb;
     arguments_definition text;
-    argument_values jsonb;
-    passed_arguments text[];
+    argument_values    jsonb;
+    passed_arguments   text[];
 begin
     if outcome is distinct from null then
         return;
@@ -52,7 +52,7 @@ begin
         if split_part(request.path, '/', 2) <> 'rpc' then
             return;
         end if;
-        call omni_rest._postgrest_function (request, function_reference, namespace, settings);
+        call omni_rest._postgrest_function(request, function_reference, namespace, settings);
         if function_reference is null then
             return;
             -- terminate
@@ -61,71 +61,66 @@ begin
         return;
         -- terminate;
     end if;
-    select
-        case request.method
-        when 'GET' then
-        (
-            select
-                array_agg(name)
-            from
-                unnest(omni_web.parse_query_string (request.query_string))
-                with ordinality as _ (name, i)
-            where
-                i % 2 = 1)
-        when 'POST' then
-        (
-            select
-                array_agg(jsonb_object_keys)
-            from
-                jsonb_object_keys(convert_from(request.body, 'utf-8')::jsonb))
-        end into passed_arguments;
-    arguments_definition := coalesce(omni_rest._postgrest_function_call_arguments (function_reference, passed_arguments), '');
+    select case request.method
+               when 'GET' then
+                   (select array_agg(name)
+                    from unnest(omni_web.parse_query_string(request.query_string))
+                             with ordinality as _ (name, i)
+                    where i % 2 = 1)
+               when 'POST' then
+                   (select array_agg(jsonb_object_keys)
+                    from
+                        jsonb_object_keys(convert_from(request.body, 'utf-8')::jsonb))
+               end
+    into passed_arguments;
+    arguments_definition :=
+            coalesce(omni_rest._postgrest_function_call_arguments(function_reference, passed_arguments), '');
     if (select proretset from pg_proc where oid = function_reference) then
-        query := format('select jsonb_agg(row_to_json(r.*)::jsonb) as result from %1$s(%2$s) r', function_reference, arguments_definition);
+        query := format('select jsonb_agg(row_to_json(r.*)::jsonb) as result from %1$s(%2$s) r', function_reference,
+                        arguments_definition);
     else
         query := format('select %1$s(%2$s) as result', function_reference, arguments_definition);
     end if;
     if request.method = 'GET' then
         query := format('set transaction read only; %1$s', query);
-        select
-            (
-                select
-                    jsonb_object_agg(keys.key, values.value)
-                from
-                    unnest(omni_web.parse_query_string (request.query_string))
-                    with ordinality as keys (key, ikey)
-                    join unnest(omni_web.parse_query_string (request.query_string))
-                    with ordinality as
-                values (value,
-                    ivalue) on ikey % 2 = 1
+        select (select jsonb_object_agg(keys.key, values.value)
+                from unnest(omni_web.parse_query_string(request.query_string))
+                         with ordinality as keys (key, ikey)
+                         join unnest(omni_web.parse_query_string(request.query_string))
+                    with ordinality as values (value,
+                                               ivalue) on ikey % 2 = 1
                     and ivalue % 2 = 0
-                    and ikey = ivalue - 1) into argument_values;
-    else
-        select
-            (
-                select
-                    convert_from(request.body, 'utf-8')::jsonb)
+                    and ikey = ivalue - 1)
         into argument_values;
-end if;
-        -- Run it
-        declare message text;
-        detail text;
-        hint text;
-        begin
-            select
-                omni_sql.execute (query, coalesce(omni_rest._postgrest_function_ordered_argument_values (function_reference, passed_arguments, argument_values), '[]'::jsonb)) -> 'result' into result;
-            if jsonb_typeof(result) = 'array' then
-              outcome := omni_httpd.http_response (result, headers => array[omni_http.http_header ('Content-Range', '0-' || jsonb_array_length(result) || '/*')]);
-            else
-              outcome := omni_httpd.http_response (result);
-            end if;
-        exception
-            when others then
-                get stacked diagnostics message = message_text,
+    else
+        select (select convert_from(request.body, 'utf-8')::jsonb)
+        into argument_values;
+    end if;
+    -- Run it
+    declare
+        message text;
+        detail  text;
+        hint    text;
+    begin
+        select omni_sql.execute(query, coalesce(
+                omni_rest._postgrest_function_ordered_argument_values(function_reference, passed_arguments,
+                                                                      argument_values), '[]'::jsonb)) -> 'result'
+        into result;
+        if jsonb_typeof(result) = 'array' then
+            outcome := omni_httpd.http_response(result, headers => array [omni_http.http_header('Content-Range', '0-' ||
+                                                                                                                 jsonb_array_length(result) ||
+                                                                                                                 '/*')]);
+        else
+            outcome := omni_httpd.http_response(result);
+        end if;
+    exception
+        when others then
+            get stacked diagnostics message = message_text,
                 detail = pg_exception_detail,
                 hint = pg_exception_hint;
-            outcome := omni_httpd.http_response (jsonb_build_object('message', message, 'detail', detail, 'hint', hint), status => 400);
-        end;
+            outcome := omni_httpd.http_response(jsonb_build_object('message', message, 'detail', detail, 'hint', hint),
+                                                status => 400);
+    end;
 end;
 $$;
 
