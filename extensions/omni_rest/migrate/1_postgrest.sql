c9acbec15998681df400ad2d1dadd3f11ac99b12
Yurii Rashkovskii <yrashk@gmail.com>
2024-12-31 10:31:43 +0700

    Problem: installing multiple versions of omni_rest
    Currently, this is impossible, but it may be desirable when multiple
    endpoints co-exist in the same database.
    
    Solution: make omni_rest a template extension

--- extensions/omni_rest/migrate/1_postgrest.sql
+++ extensions/omni_rest/migrate/1_postgrest.sql
@@ -1,17 +1,2 @@
-create type postgrest_settings as
-(
-    -- The only schemas `postgrest` is allowed to access
-    schemas name[]
-);
-
-/*{% include "../src/postgrest_settings.sql" %}*/
-/*{% include "../src/_postgrest_relation.sql" %}*/
-/*{% include "../src/_postgrest_function.sql" %}*/
-
-/*{% include "../src/__eval.sql" %}*/
-/*{% include "../src/postgrest_get.sql" %}*/
-/*{% include "../src/postgrest_rpc.sql" %}*/
-/*{% include "../src/postgrest_insert.sql" %}*/
-/*{% include "../src/postgrest_update.sql" %}*/
-/*{% include "../src/postgrest_cors.sql" %}*/
-/*{% include "../src/postgrest.sql" %}*/
+/*{% include "../src/instantiate.sql" %}*/
+select instantiate(schema => '@extschema@');
