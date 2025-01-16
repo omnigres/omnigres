59d5c5de1c97d384ff2e25eaaa395c72122502b2
Diogo Biazus <diogo@biazus.ca>
2025-01-16 12:47:09 -0500

    Problem: make_plan was not taking pre-processing done by assemble_schema. This would happen with auxiliary tools and files that are not SQL. The initial implementation would just break an bail out.
    Solution: once we have assemble_schema outputting the actual executed
    statement we can use this already pre-processed version to make our
    plan.
    
    As a consequence it was easier to bring syntax errors from
    assemble_schema output as well instead of re-validating (as the previous
    implementation was doing).

--- extensions/omni_schema/src/make_plan.sql
+++ extensions/omni_schema/src/make_plan.sql
@@ -16,40 +16,65 @@ declare
     stmt omni_sql.statement;
     error_message text;
     hint_message text;
+    extension_record record;
 begin
     perform dblink(current_db_conn, format('create database %I', plan_db));
     perform dblink(plan_db_conn, 'create extension omni_schema cascade;');
+
+    -- prepare necessary extensions in the remote database
+    for extension_record in select distinct extension
+               from omni_schema.languages
+               union
+               select distinct file_processor_extension as extension
+               from omni_schema.languages
+               union
+               select distinct processor_extension as extension
+               from omni_schema.auxiliary_tools
+    loop
+            if exists (select from pg_extension where extname = extension_record.extension) then
+                perform dblink(plan_db_conn,
+                               format('create extension if not exists %s cascade', extension_record.extension));
+            end if;
+    end loop;
+
     for migration in select * from dblink(
         plan_db_conn,
-        format('select migration_filename, migration_statement from omni_schema.assemble_schema(%L, omni_vfs.local_fs(%L))', plan_db_conn, path)
-    ) migrations (migration_filename text, migration_statement text)
+        format('select migration_filename, executed_statement, migration_statement, execution_error from omni_schema.assemble_schema(%L, omni_vfs.local_fs(%L))', plan_db_conn, path)
+    ) migrations (migration_filename text, executed_statement text, migration_statement text, execution_error text)
     loop
-        begin
-            stmt := migration.migration_statement::omni_sql.statement;
-        exception
-            when others then
-                get stacked diagnostics error_message = message_text,
-                              hint_message = pg_exception_hint;
-                raise exception 'Error "%" in %', error_message, migration.migration_filename
-                    using 
-                        detail = migration.migration_statement,
-                        hint = hint_message;
-        end;
+        if migration.executed_statement is not null and migration.execution_error is null then
+            begin
+                stmt := migration.executed_statement::omni_sql.statement;
+            exception
+                when others then
+                    get stacked diagnostics error_message = message_text,
+                                  hint_message = pg_exception_hint;
+                    raise exception 'Error "%" in %', error_message, migration.migration_filename
+                        using 
+                            detail = migration.executed_statement,
+                            hint = hint_message;
+            end;
+        elsif migration.execution_error is not null then
+            raise exception '% in %', migration.execution_error, migration.migration_filename
+                using 
+                    detail = migration.migration_statement;
+        end if;
 
         if not (
             migration.migration_filename ~* 'migrations' 
-            or omni_sql.is_replace_statement(migration.migration_statement::omni_sql.statement) 
+            or migration.executed_statement ~* 'select omni_python.install_requirements'
+            or omni_sql.is_replace_statement(migration.executed_statement::omni_sql.statement) 
         )
         then
             raise exception 'Non replace statement found outside migrations directory in %', migration.migration_filename
                 using 
-                    detail = migration.migration_statement,
+                    detail = migration.executed_statement,
                     hint = 'Move file into migrations or ensure it contains only statements with OR REPLACE';
         end if;
 
         if not exists(select from omni_schema.migrations m where lower(m.name) = lower(omni_vfs.basename(migration.migration_filename))) 
         then
-            return query select migration.migration_filename, migration.migration_statement::omni_sql.statement;
+            return query select migration.migration_filename, migration.executed_statement::omni_sql.statement;
         end if;
     end loop;
 
