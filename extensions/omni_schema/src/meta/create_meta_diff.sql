4710fa622a80e4188fe7ada6af63446a5dec7a7a
Yurii Rashkovskii <yrashk@gmail.com>
2025-01-29 10:37:28 -0800

    Problem: diffing more complicated changes
    Now that we've done some basic table stuff with a fairly small surface, we need
    to start covering other meta-information.
    
    However, it becomes less trivial as we need to be able to identify changes
    without comparing rows.
    
    Solution: go 6NF (sixths normal form) on the meta design
    
    The reason for this is that with 6NF we can identify change of every particular
    fact very easily without doing row by row comparison.
    
    To this effect, this removes baseline/changed views as added/changed is all you
    need.
    
    This changes an underlying assumption on the integrated `meta` code, and it'll
    continue diverging from it as we change it. Since we've already had tables, I
    had to go back and make sure the design aligns.
    
    I've also enabled pg_catalog types to be included in the `type` view for
    referential integrity.

--- extensions/omni_schema/src/meta/create_meta_diff.sql
+++ extensions/omni_schema/src/meta/create_meta_diff.sql
@@ -24,26 +24,14 @@ begin
         loop
             execute format($sql$
     create or replace view added_%3$s as select b.* from %2$I.%3$I b
-        left outer join %1$I.%3$I a on a.id = b.id
+        natural left outer join %1$I.%3$I a
         where a is not distinct from null
     $sql$, baseline, changed, type);
 
             execute format($sql$
     create or replace view removed_%3$s as select b.* from %1$I.%3$I b
-        left outer join %2$I.%3$I a on a.id = b.id
+        natural left outer join %2$I.%3$I a
         where a is not distinct from null
-    $sql$, baseline, changed, type);
-
-            execute format($sql$
-    create or replace view baseline_%3$s as select a.* from %2$I.%3I b
-        inner join %1$I.%3$I a on a.id = b.id
-        where a != b
-    $sql$, baseline, changed, type);
-
-            execute format($sql$
-    create or replace view changed_%3$s as select b.* from %2$I.%3I b
-        inner join %1$I.%3$I a on a.id = b.id
-        where a != b
     $sql$, baseline, changed, type);
 
         end loop;
