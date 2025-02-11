00e1ce8069bfb2269aa1c1059444e67468216a0d
Yurii Rashkovskii <yrashk@omnigres.com>
2025-02-11 06:54:08 -0800

    Problem: tracking schema evolution
    Tracking schema evolution, especially in presence of data-native functions
    (fka _stored procedures_) is a non-trivial task:
    
    * How do we prevent data and auxiliary information (such as presence of indices) loss?
    * How do we ensure that schema is evolved as intended?
    * How do we make sure the correct set of extensions at correct versions is used for every specific version?
    * How do we sure data-native functions correspond the correct revision of the schema?
    * and so on.
    
    Solution: implement a robust tracking solution
    following [RFC0001](https://github.com/omnigres/rfc/pull/1)

--- extensions/omni_schema/src/meta.sql
+++ extensions/omni_schema/src/meta.sql
@@ -1,40 +1,6 @@
-/*{%
-  set meta_views = ["schema",
-                    "cast","cast_binary_coercible","cast_function","cast_implicit","cast_implicit_in_assignment","cast_explicit",
-                    "operator",
-                    "sequence", "sequence_minimum_value", "sequence_start_value", "sequence_maximum_value",
-                                "sequence_cycle", "sequence_increment", "sequence_type", "sequence_cache",
-                                "sequence_table",
-                    "relation_column","relation_column_type","relation_column_position","relation_column_default",
-                                      "relation_column_nullable","relation",
-                    "table","table_permanent", "table_temporary", "table_partitioned", "table_rowsecurity", "table_forcerowsecurity",
-                    "view",
-                    "callable", "callable_function", "callable_procedure", "callable_return_type", "callable_owner",
-                                "callable_argument_name", "callable_argument_type", "callable_argument_mode", "callable_argument_default",
-                                "callable_immutable", "callable_volatile", "callable_stable",
-                                "callable_aggregate", "callable_window",
-                                "callable_parallel_safe", "callable_parallel_unsafe", "callable_parallel_restricted",
-                                "callable_security_definer", "callable_security_invoker","callable_language",
-                                "callable_acl", "callable_body",
-                    "trigger",
-                    "role","role_inheritance",
-                    "table_privilege",
-                    "policy","policy_role",
-                    "constraint_relation_non_null","constraint_relation_non_null_column",
-                    "constraint_relation_check", "constraint_relation_check_expr",
-                    "constraint_relation_unique","constraint_relation_unique_column",
-                    "extension",
-                    "foreign_data_wrapper","foreign_server","foreign_table","foreign_column",
-                    "foreign_key",
-                    "type", "type_basic", "type_array", "type_composite","type_composite_attribute",
-                            "type_composite_attribute_position","type_composite_attribute_collation",
-                            "type_domain","type_enum","type_enum_label", "type_pseudo", "type_range","type_multirange",
-                    "index","index_relation", "index_unique", "index_unique_null_values_distinct",
-                            "index_primary_key", "index_unique_immediate", "index_replica_identity",
-                            "index_attribute","index_partial"
-                    ]
- %}*/
+/*{% extends "meta_views.sql" %}*/
 
+/*{% block content %}*/
 create function instantiate_meta(schema name) returns void
     language plpgsql
 as
@@ -73,3 +39,4 @@ begin
 
 end;
 $instantiate_meta$;
+/*{% endblock %}*/
