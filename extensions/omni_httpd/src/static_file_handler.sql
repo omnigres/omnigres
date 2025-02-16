631d2060caa0b2cf9d44a425a88ce067139eb1fa
Yurii Rashkovskii <yrashk@gmail.com>
2025-02-16 08:56:00 -0800

    Problem: omni_httpd static file handler is a bit rigid
    In particular, it'll always look for request.path for the path, but in
    the case of SPA that's not what we always want.
    
    Solution: allow specifying path explicitly

--- extensions/omni_httpd/src/static_file_handler.sql
+++ extensions/omni_httpd/src/static_file_handler.sql
@@ -1,24 +1,28 @@
 create
     or
     replace function static_file_handler(request omni_httpd.http_request,
-                                         fs anyelement, path text default '', listing boolean default false)
+                                         fs anyelement, fs_path text default '', path text default null,
+                                         listing boolean default false)
     returns omni_httpd.http_outcome
     language plpgsql as
 $static_file_handler$
 declare
     outcome omni_httpd.http_outcome;
 begin
+    if path is null then
+        path := request.path;
+    end if;
     --- File
     if request.method = 'GET' and
-       (omni_vfs.file_info(fs, path || request.path)).kind = 'file' then
+       (omni_vfs.file_info(fs, fs_path || path)).kind = 'file' then
         select
             omni_httpd.http_response(
-                    omni_vfs.read(fs, path || request.path),
+                    omni_vfs.read(fs, fs_path || path),
                     headers => array [omni_http.http_header('content-type',
                                                             coalesce(mime_types.name, 'application/octet-stream'))]::omni_http.http_header[])
         from
                 (select)                                            _
-                left join omni_mimetypes.file_extensions on request.path like '%%.' || file_extensions.extension
+                    left join omni_mimetypes.file_extensions on path like '%%.' || file_extensions.extension
                 left join omni_mimetypes.mime_types_file_extensions mtfe on mtfe.file_extension_id = file_extensions.id
                 left join omni_mimetypes.mime_types on mtfe.mime_type_id = mime_types.id
         into outcome;
@@ -30,12 +34,12 @@ begin
 
     --- Directory
     if request.method = 'GET' and
-       ((request.path = '/') or
-        (omni_vfs.file_info(fs, path || request.path)).kind = 'dir') and
-       (omni_vfs.file_info(fs, path || request.path || '/index.html')).kind = 'file' then
+       ((path = '/') or
+        (omni_vfs.file_info(fs, fs_path || path)).kind = 'dir') and
+       (omni_vfs.file_info(fs, fs_path || path || '/index.html')).kind = 'file' then
         select
             omni_httpd.http_response(
-                    omni_vfs.read(fs, path || request.path || '/index.html'),
+                    omni_vfs.read(fs, fs_path || path || '/index.html'),
                     headers => array [omni_http.http_header('content-type', 'text/html')]::omni_http.http_header[])
         into outcome;
     end if;
@@ -47,17 +51,16 @@ begin
     --- File listing
 
     if listing and request.method = 'GET' and
-       ((request.path = '/') or
-        (omni_vfs.file_info(fs, path || request.path)).kind = 'dir') and
-       (omni_vfs.file_info(fs, path || request.path || '/index.html')) is not distinct from null then
+       ((path = '/') or
+        (omni_vfs.file_info(fs, fs_path || path)).kind = 'dir') and
+       (omni_vfs.file_info(fs, fs_path || path || '/index.html')) is not distinct from null then
         select
             omni_httpd.http_response(
-                    (select
-                         string_agg('<a href="' || case when request.path = '/' then '/' else request.path || '/' end ||
+                    (select string_agg('<a href="' || case when path = '/' then '/' else path || '/' end ||
                                     name ||
                                     '">' || name || '</a>', '<br>')
                      from
-                         omni_vfs.list(fs, path || request.path)),
+                         omni_vfs.list(fs, fs_path || path)),
                     headers => array [omni_http.http_header('content-type', 'text/html')]::omni_http.http_header[])
 
         into outcome;
