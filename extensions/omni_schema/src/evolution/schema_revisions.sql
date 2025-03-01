bbd46ce4e11fb0598f2837033fad53996b027927
Yurii Rashkovskii <yrashk@omnigres.com>
2025-02-28 17:27:02 -0800

    Problem: omni_schema.schema_revisions are unordered
    This is not most convenient.
    
    Solution: order them from the oldest to newest

--- extensions/omni_schema/src/evolution/schema_revisions.sql
+++ extensions/omni_schema/src/evolution/schema_revisions.sql
@@ -8,8 +8,6 @@ create function schema_revisions(fs anyelement, revisions_path text, leafs_only 
     language plpgsql
 as
 $$
-declare
-    rec record;
 begin
     if leafs_only then
         return query
@@ -24,24 +22,52 @@ begin
             where
                 parent is not distinct from null;
     else
-        for rec in select *
-                   from
-                       omni_vfs.list_recursively(fs, revisions_path)
-                   where
-                       omni_vfs.basename(name) = 'metadata.yaml'
-            loop
-                revision := omni_vfs.dirname(rec.name)::uuid;
-                metadata := omni_yaml.to_json(convert_from(
-                        omni_vfs.read(fs, revisions_path || '/' || rec.name), 'utf-8'));
-                parents := array((select
-                                      jsonb_array_elements_text(coalesce(
-                                              metadata ->
-                                              'parents',
-                                              '[]'))))::revision_id[];
-                return next;
-            end loop;
-
-        return;
+        return query with
+                         recursive
+                         revisions as (select
+                                           omni_vfs.dirname(name)::uuid::revision_id  as revision,
+                                           omni_yaml.to_json(convert_from(
+                                                   omni_vfs.read(fs, revisions_path || '/' || name),
+                                                   'utf-8'))::jsonb                   as metadata,
+                                           array((select
+                                                      jsonb_array_elements_text(coalesce(
+                                                              (omni_yaml.to_json(convert_from(
+                                                                      omni_vfs.read(fs, revisions_path || '/' || name),
+                                                                      'utf-8'))::jsonb) ->
+                                                              'parents',
+                                                              '[]'))))::revision_id[] as parents
+                                       from
+                                           omni_vfs.list_recursively(fs, revisions_path)
+                                       where
+                                           omni_vfs.basename(name) = 'metadata.yaml'),
+                         all_ancestors as (select
+                                               r.revision,
+                                               unnest(r.parents) as ancestor
+                                           from
+                                               revisions r
+                                           union all
+                                           select
+                                               aa.revision,
+                                               unnest(r.parents) as ancestor
+                                           from
+                                               all_ancestors  aa
+                                               join revisions r
+                                                    on aa.ancestor = r.revision),
+                         descendant_counts as (select
+                                                   ancestor                    as parent,
+                                                   count(distinct aa.revision) as descendant_count
+                                               from
+                                                   all_ancestors aa
+                                               group by ancestor)
+                     select
+                         r.revision as revision,
+                         r.parents  as parents,
+                         r.metadata as metadata
+                     from
+                         revisions                   r
+                         left join descendant_counts dc
+                                   on dc.parent = r.revision
+                     order by coalesce(dc.descendant_count, 0) desc;
     end if;
 end;
 $$;
