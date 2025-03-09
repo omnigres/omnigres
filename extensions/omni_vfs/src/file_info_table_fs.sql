7f8bdb58b832557687bbc2a0452ad5639442125c
Ayush Jhawar <ayushjhawar499@gmail.com>
2025-03-09 14:14:00 +0000

    fix: omni_vfs.file_info for table_fs on root

--- extensions/omni_vfs/src/file_info_table_fs.sql
+++ extensions/omni_vfs/src/file_info_table_fs.sql
@@ -3,9 +3,17 @@ create function file_info(fs table_fs, path text) returns omni_vfs_types_v1.file
     language sql set search_path to omni_vfs
 as
 $$
-
+-- Special case for root directory
 with
-    match(id) as (select table_fs_file_id(fs, path))
+    match(id) as (
+        select 
+            case 
+                when omni_vfs.canonicalize_path(path, absolute => true) = '/' then 
+                    (select id from table_fs_files where filesystem_id = fs.id and filename = '' and depth = 1)
+                else 
+                    table_fs_file_id(fs, path)
+            end
+    )
 select
     coalesce(length(d.data), 0) as size,
     d.created_at,
@@ -15,7 +23,7 @@ select
 from
     table_fs_files                f
     inner join match              m on f.id = m.id
-    inner join table_fs_file_data d on m.id = d.file_id
+    left join table_fs_file_data  d on m.id = d.file_id
 where
     filesystem_id = fs.id
 $$;
