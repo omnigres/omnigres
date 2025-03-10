7fa4e46bb2af865472303db3352858587f6dbf60
Yurii Rashkovskii <yrashk@omnigres.com>
2025-03-10 16:57:28 -0700

    Problem: generializing omni_credentials
    When we want to use them across different subsystems and extensions, they lack
    specificity. It's just an encrypted key/value store.
    
    It's not clear what naming convention should be used, for example.
    
    Solution: expand the concept to include further metadata
    
    "What kind of credential?" – `kind`
    "Who can have access to it?" – `principal`
    "What is the scope of the credential?" – `scope`
    
    This will allow much better "notation". Since we're getting all these details,
    it now makes sense to enable RLS on this table and by default, filter by principal.
    Users can add more policies to it. The reason why this metadata is not factored out
    is so that RLS _can_ be used there without sub-queries.
    
    This removes the `credentials_master_password` function that was effectively leaking the
    encryption key.

--- extensions/omni_credentials/src/instantiate.sql
+++ extensions/omni_credentials/src/instantiate.sql
@@ -12,45 +12,137 @@ begin
         unique (environment_variable)
     );
 
-    insert into credentials_config (environment_variable) values (env_var);
+    insert
+    into
+        credentials_config (environment_variable)
+    values (env_var);
+
+    create type credential_kind as enum ('credential','api_key','api_secret','password', 'token', 'token[jwt]', 'token[oauth]', 'ssh','certificate', 'otp', 'saml_assertion');
 
     create table encrypted_credentials
     (
-        name  text  not null unique,
-        value bytea not null
+        name      text            not null,
+        value     bytea           not null,
+        kind      credential_kind not null default 'credential',
+        principal regrole         not null default current_user::regrole,
+        scope     jsonb           not null default '{
+          "all": true
+        }'
     );
+    alter table encrypted_credentials
+        enable row level security;
+    create policy encrypted_credentials_principal on encrypted_credentials
+        using (pg_has_role(current_user, principal, 'USAGE'));
 
-    create function credentials_master_password() returns text
-        language sql
-    as
-    $sql$
-    select value
-    from omni_os.env
-             inner join credentials_config on true
-    where variable = environment_variable
-    $sql$;
-    execute format('alter function credentials_master_password set search_path to %I', schema);
+    create unique index encrypted_credentials_uniq
+        on encrypted_credentials (name, kind, principal, scope);
+
+    create function decrypt_credential(credential bytea) returns text
+        security definer
+    begin
+        atomic
+        select
+            pgp_sym_decrypt(credential, value)
+        from
+            omni_os.env
+            inner join credentials_config on true
+        where
+            variable = environment_variable;
+    end;
+
+    create function encrypt_credential(credential text) returns bytea
+        security definer
+    begin
+        atomic
+        select
+            pgp_sym_encrypt(credential, value)
+        from
+            omni_os.env
+            inner join credentials_config on true
+        where
+            variable = environment_variable;
+    end;
 
 
-    create view credentials as
-    select name,
-           pgp_sym_decrypt(value, credentials_master_password()) as value
-    from encrypted_credentials;
+    if current_setting('server_version_num')::int / 10000 > 14 then
+        create view credentials with (security_barrier, security_invoker) as
+            select
+                name,
+                decrypt_credential(value) as value,
+                kind,
+                principal,
+                scope
+            from
+                encrypted_credentials;
+    else
+        create function _credentials_view()
+            returns table
+                    (
+                        name      text,
+                        value     text,
+                        kind      credential_kind,
+                        principal regrole,
+                        scope     jsonb
+                    )
+            security invoker
+        begin
+            atomic
+            select
+                name,
+                decrypt_credential(value) as value,
+                kind,
+                principal,
+                scope
+            from
+                encrypted_credentials;
+        end;
+        create view credentials with (security_barrier) as
+            select * from _credentials_view();
+    end if;
+
+    alter table credentials
+        alter column kind set default 'credential';
+    alter table credentials
+        alter column principal set default current_user::regrole;
+    alter table credentials
+        alter column scope set default '{"all": true}';
 
     create function credentials_update() returns trigger
         security definer
         language plpgsql as
     $code$
     begin
+        if tg_op = 'DELETE' then
+            delete
+            from
+                encrypted_credentials
+            where
+                name = old.name and
+                kind = old.kind and
+                principal = old.principal and
+                scope = old.scope;
+            return old;
+        end if;
         if old is distinct from null then
             update encrypted_credentials
-            set value = pgp_sym_encrypt(new.value, credentials_master_password()),
-                name  = new.name
-            where name = old.name;
+            set
+                value = encrypt_credential(new.value),
+                name  = new.name,
+                kind = new.kind,
+                principal = new.principal,
+                scope = new.scope
+            where
+                name = old.name and
+                kind = old.kind and
+                principal = old.principal and
+                scope = old.scope;
         else
             insert
-            into encrypted_credentials (name, value)
-            values (new.name, pgp_sym_encrypt(new.value, credentials_master_password()));
+            into
+                encrypted_credentials (name, value, kind, principal, scope)
+            values
+                (new.name, encrypt_credential(new.value), new.kind, new.principal,
+                 new.scope);
         end if;
         return new;
     end;
@@ -58,7 +150,7 @@ begin
     execute format('alter function credentials_update set search_path to %I,public', schema);
 
     create trigger credentials_update
-        instead of update or insert
+        instead of update or insert or delete
         on credentials
         for each row
     execute function credentials_update();
