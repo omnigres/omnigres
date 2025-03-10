5ec07c711285c3f2a14c5664b7fdce54766e6185
deekshatomer <deekshatomer951@gmail.com>
2025-03-10 16:31:10 +0530

    Fix file_info to return non-null timestamps for root directory

--- extensions/omni_vfs/src/file_info_table_fs.sql
+++ extensions/omni_vfs/src/file_info_table_fs.sql
@@ -1,21 +1,26 @@
-create function file_info(fs table_fs, path text) returns omni_vfs_types_v1.file_info
-    stable
-    language sql set search_path to omni_vfs
-as
+CREATE OR REPLACE FUNCTION omni_vfs.file_info(fs omni_vfs.table_fs, path TEXT) RETURNS omni_vfs_types_v1.file_info
+    STABLE
+    LANGUAGE sql SET search_path TO omni_vfs
+AS
 $$
 
-with
-    match(id) as (select table_fs_file_id(fs, path))
-select
-    coalesce(length(d.data), 0) as size,
-    d.created_at,
-    d.accessed_at,
-    d.modified_at,
-    f.kind
-from
-    table_fs_files                f
-    inner join match              m on f.id = m.id
-    inner join table_fs_file_data d on m.id = d.file_id
-where
-    filesystem_id = fs.id
+WITH
+    match(id) AS (
+        SELECT omni_vfs.table_fs_file_id(fs, path)
+        UNION ALL
+        SELECT NULL WHERE path = '/'
+    )
+SELECT
+   COALESCE(LENGTH(d.data), 0) AS size,
+    COALESCE(d.created_at, NOW()) AS created_at,
+    COALESCE(d.accessed_at, NOW()) AS accessed_at,
+    COALESCE(d.modified_at, NOW()) AS modified_at,
+    COALESCE(f.kind, 'dir') AS kind
+FROM
+    omni_vfs.table_fs_files                f
+    FULL OUTER JOIN match         m ON f.id = m.id
+    FULL OUTER JOIN omni_vfs.table_fs_file_data d ON m.id = d.file_id
+WHERE
+ (f.filesystem_id = fs.id OR m.id IS NULL);
+
 $$;
