80e15688aca0cdbcbb6fedab7724135f37f2e867
oliver <8559757+oliverqx@users.noreply.github.com>
2025-03-18 09:25:00 -0600

    Problem: Current vfs implementation ignores several columns when exporting credentials.
    Solution: Export all columns from the table.

--- extensions/omni_credentials/src/instantiate_file_store.sql
+++ extensions/omni_credentials/src/instantiate_file_store.sql
@@ -75,9 +75,9 @@ begin
         r record;
         content text := '';
     begin
-        for r in select name, value from encrypted_credentials order by name
+        for r in select name, value, kind, principal, scope from encrypted_credentials order by name
         loop
-            content := content || r.name || ' ' || encode(r.value, 'base64') || E'\n';
+            content := content || r.name || ' ' || encode(r.value, 'base64') || ' ' || r.kind || ' ' || r.principal || ' ' || r.scope ||  E'\n';
         end loop;
 
         perform omni_vfs.write(
