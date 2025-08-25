b01e2e1d6c4c0a7b3b1f4ce32922aae116bedbc9
Yurii Rashkovskii <yrashk@gmail.com>
2025-08-25 11:32:31 -0700

    Problem: watching Kubernetes resources
    Right now the only operation possible is to list resources. It may be
    generally sufficient, but sometimes watching for event is more
    preferrable.
    
    Solution: introduce a limited watching capability
    
    It's not really "streaming" as it forces a timeout to finish and return
    the results (until we figure out a better model for it)
    
    This also pushed me to make it possible to make multiple API requests at
    the same time to be able to watch on multiple resource collections at once.

---
+++ extensions/omni_kube/src/watch.sql
@@ -0,0 +1,39 @@
+create function watch(group_versions text[], resources text[], resource_versions text[] default null,
+                      timeout int default 1)
+    returns table
+            (
+                events jsonb[],
+                status   int2
+            )
+begin
+    atomic;
+    with resource_versions as (select resources_metadata(unnest(group_versions), unnest(resources)) ->>
+                                      'resourceVersion' as resource_version),
+         items as (select unnest(group_versions)                                        as group_version,
+                          unnest(resources)                                             as resource,
+                          unnest(array(select resource_version from resource_versions)) as resource_version,
+                          unnest(watch.resource_versions)                               as requested_version),
+         resp as (select api(array_agg('/' ||
+                                              case
+                                                  when group_version in ('v1')
+                                                      then 'api/v1'
+                                                  else 'apis/' || group_version end ||
+                                              '/' ||
+                                              resource || '?watch=1&resourceVersion=' ||
+                                              coalesce(requested_version, resource_version) || '&timeoutSeconds=' ||
+                                       timeout), stream => true) as e
+                             from items)
+    select array(select jsonb_array_elements((e).response)), (e).status
+    from resp;
+end;
+
+create function watch(group_version text, resource text, resource_version text default null, timeout int default 1)
+    returns table
+            (
+                events jsonb[],
+                status int2
+            )
+begin
+    atomic;
+    select watch(array [group_version], array [resource], array [resource_version], timeout);
+end;
