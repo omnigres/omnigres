e1707878cbbed23879bc7c2f9850602d2a037117
Yurii Rashkovskii <yrashk@gmail.com>
2025-08-30 06:49:16 -0700

    Problem: omni's shared memory management
    It's based on DSA/DSM and is, frankly, tedious to follow. It's also
    coupled with omni's release lifecycle.
    
    Solution: reimplement it as a new extension
    
    Simpler design, based on Boost Interprocess (wrapped into our
    lightweight Oink library).
    
    Importantly, this introduces APEX: API for Postgres Extensions. It's a
    way to expose C APIs between extensions and a robust initialization
    technique. I plan to test it out with our extensions and then release it
    more publicly.
    
    Also, on testing side, since we're doing this APEX business, I am also
    now testing C/C++ level code with doctest-based tests compiled into the
    extension. The idea here is that we'll probably disable those for full
    production builds.

---
+++ extensions/omni_shmem/apex_omni_shmem.h
@@ -0,0 +1,29 @@
+#ifndef apex_omni_shmem_h
+#define apex_omni_shmem_h
+
+typedef struct omni_arena_allocation {
+  void *ptr;
+  bool found;
+} omni_arena_allocation;
+
+typedef struct omni_arena {
+  void *(*alloc)(omni_arena *, size_t size);
+  void (*free)(omni_arena *, void *ptr);
+  void *(*find)(omni_arena *, const char *name);
+  omni_arena_allocation (*find_or_construct)(omni_arena *, const char *name, size_t size,
+                                             void(destructor)(void *ptr, void *destructor_ctx),
+                                             void *destructor_ctx);
+  bool (*destroy)(omni_arena *, const char *name);
+  void (*release)(omni_arena *);
+} omni_shmem;
+
+typedef struct omni_shmem_api {
+  omni_arena *(*open)(const char *name, bool transient);
+  omni_arena *(*open_or_create)(const char *name, size_t, bool transient);
+} omni_shmem_api;
+
+#define apex_api_type omni_shmem_api
+#define apex_api "omni_shmem"
+#include <apex.h>
+
+#endif // apex_omni_shmem_h
