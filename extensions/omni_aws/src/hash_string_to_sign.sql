e7d3a31bd308e3e6e36330bd560a31dbf37a67a8
redlittenyoth <redlittenyoth@gmail.com>
2026-02-13 08:43:43 +0300

    refactor(omni_aws): move hash_string_to_sign to plpgsql for readability

--- extensions/omni_aws/src/hash_string_to_sign.sql
+++ extensions/omni_aws/src/hash_string_to_sign.sql
@@ -6,25 +6,23 @@ create function hash_string_to_sign(
     canonical_request_hash text,
     secret_access_key text)
     returns text
-    language sql
+    language plpgsql
     immutable
 as
 $$
-select
-    encode(hmac((((((((algorithm || chr(10) ||
-                       to_char((ts8601 at time zone 'UTC'), 'YYYYMMDD"T"HH24MISS"Z"') || chr(10) ||
-                       to_char((ts8601 at time zone 'UTC'), 'YYYYMMDD')) || '/' || region) || '/') ||
-                    service) || '/aws4_request') || chr(10)) || canonical_request_hash)::bytea, hmac('aws4_request',
-                                                                                                     hmac(
-                                                                                                             service::bytea,
-                                                                                                             hmac(
-                                                                                                                     region::bytea,
-                                                                                                                     hmac(
-                                                                                                                             to_char((ts8601 at time zone 'UTC'), 'YYYYMMDD'),
-                                                                                                                             ('AWS4' || secret_access_key),
-                                                                                                                             'sha256'),
-                                                                                                                     'sha256'),
-                                                                                                             'sha256'),
-                                                                                                     'sha256'),
-                'sha256'), 'hex')
+declare
+    date_text      text  := to_char((ts8601 at time zone 'UTC'), 'YYYYMMDD');
+    k_date         bytea := hmac(date_text, ('AWS4' || secret_access_key), 'sha256');
+    k_region       bytea := hmac(region::bytea, k_date, 'sha256');
+    k_service      bytea := hmac(service::bytea, k_region, 'sha256');
+    k_signing      bytea := hmac('aws4_request', k_service, 'sha256');
+    string_to_sign text;
+begin
+    string_to_sign := algorithm || chr(10) ||
+                      to_char((ts8601 at time zone 'UTC'), 'YYYYMMDD"T"HH24MISS"Z"') || chr(10) ||
+                      date_text || '/' || region || '/' || service || '/aws4_request' || chr(10) ||
+                      canonical_request_hash;
+
+    return encode(hmac(string_to_sign::bytea, k_signing, 'sha256'), 'hex');
+end;
 $$;
